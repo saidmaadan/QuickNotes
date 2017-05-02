4b22dee6fea50e7b3db1187dc997f69e67b416b9
Said Maadan <me@saidmaadan.com>
2017-05-01 20:01:14 -0500

    Created required pages and installed platforms and plugins

---
+++ src/providers/data.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the Data provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class Data {
+
+  constructor() {
+    console.log('Hello Data Provider');
+  }
+
+}
