7704a31ee202dbb8a73145bff55aedae30dbd9bc
Said Maadan <me@saidmaadan.com>
2017-05-01 22:35:58 -0500

    Created data model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from "@ionic/storage";
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Keyboard } from '@ionic-native/keyboard';
 
 import { MyApp } from './app.component';
 import { Data } from '../providers/data';
 
 @NgModule({
   declarations: [
-    MyApp,
+    MyApp
 
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    MyApp
 
   ],
   providers: [
     Data,
     StatusBar,
     SplashScreen,
+    Keyboard,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
